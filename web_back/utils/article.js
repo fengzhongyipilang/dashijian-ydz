ecd9033c2eca79263a8f94ade17c568bbde2d856
ydz <yaodazhen.com>
2019-12-06 22:56:16 +0800

    热点图加载

--- web_back/utils/article.js
+++ web_back/utils/article.js
@@ -1,10 +1,15 @@
-var category = {
+var article = {
     show: function (callback) {  
     $.get(apis.category_show,function(res){
      callback(res);
     })
-    }
-     
+    },
+      
+//热点图
+hotpic: function (callback) {
+        $.get(apis.article_hotpic,function (res) { 
+            callback(res);
+         })
+     },
 
-    
 }
--- web_front/utils/config.js
+++ web_front/utils/config.js
@@ -4,6 +4,7 @@ var baseUrl = 'http://localhost:8000/';
 var apis = {
     // 分类相关接口
     category_show: baseUrl + 'category',
+    article_hotpic: baseUrl + 'hotpic',
 
     // 文章相关接口
   
